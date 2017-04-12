a7e534461c01bfbac626464eb6bcbd28147d6ebc
SilentSupernova <chrisyoules93@gmail.com>
2017-04-12 19:53:27 +0100

    Crate model and shit

---
+++ RogueLike/Assets/Scripts/WeaponCrate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCrate : MonoBehaviour {
+
+    public GameObject WeaponPickUp;
+    private MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void CrateDestroy()
+    {
+        meshRenderer.enabled = false;
+        WeaponPickUp.SetActive(true);
+    }
+}
