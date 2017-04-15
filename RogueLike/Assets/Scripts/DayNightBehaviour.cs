6b0c5ea29d9a0f5ad0d1fcc7584c9fdd8cce39a9
David Greedy <DavidJamesGreedy@gmail.com>
2017-04-15 23:44:45 +0100

    Added Day/Night cycle

---
+++ RogueLike/Assets/Scripts/DayNightBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightBehaviour : MonoBehaviour
+{
+    public Light m_sunLight;
+
+    public float m_dayLength; // Time in seconds
+
+    public int m_currentDay;
+
+    public Vector3 m_axis;
+
+    private float m_daySpeed;
+
+    private float startTime;
+
+    [SerializeField, Range(0f, 1f)]
+    private float m_dayNight;
+
+    [SerializeField]
+    private float time;
+
+    [SerializeField]
+    private bool isDay;
+
+    [SerializeField]
+    private string s;
+
+    void Start()
+    {
+        m_currentDay = 0;
+        isDay = true;
+    }
+
+    private string GetTimeOfDay()
+    {
+        TimeSpan t = TimeSpan.FromSeconds(time * 60f * 60f * 24f);
+        return string.Format("{0:D2}h:{1:D2}m:{2:D2}s", t.Hours, t.Minutes, t.Seconds);
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime / m_dayLength;
+        isDay = time < m_dayNight;
+        m_sunLight.transform.rotation = Quaternion.AngleAxis(isDay ? Mathf.Lerp(0, 180, time / m_dayNight) : Mathf.Lerp(180, 360, time / (1 - m_dayNight)), m_axis.normalized);
+        if (time > 1.0f)
+        {
+            m_currentDay++;
+            time = 0;
+        }
+        s = GetTimeOfDay();
+    }
+}
